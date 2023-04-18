0e95ad0052e3b8683dc734c0d288fdf37e934306
jmwehipeihana <129016239+jmwehipeihana@users.noreply.github.com>
2023-04-18 22:30:44 +1200

    Add files via upload
    Assessment 2

---
+++ scpv2/data.js
@@ -0,0 +1,63 @@
+// Define a function to load the SCP data
+function loadSCPData(callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.overrideMimeType("application/json");
+  xhr.open("GET", "scp.json", true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const scpData = JSON.parse(xhr.responseText);
+      callback(scpData);
+    }
+  };
+  xhr.send(null);
+}
+
+function displaySCP(scpData, scpNumber) {
+  const scp = scpData[`SCP-${scpNumber}`];
+  const scpHTML = `
+    <h2>${scp.subject}</h2>
+    <p><strong>Class:</strong> ${scp.class}</p>
+    <p><strong>Description:</strong> ${scp.description}</p>
+    <p><strong>Special Containment Procedures:</strong> ${scp.Containment}</p>
+  `;
+  document.getElementById(`scp${scpNumber}`).innerHTML = scpHTML;
+}
+
+// Check if Speech Synthesis API is supported
+if ('speechSynthesis' in window) {
+  console.log('Speech Synthesis API is supported.');
+} else {
+  console.log('Speech Synthesis API is not supported.');
+}
+
+const synth = window.speechSynthesis;
+
+const readDescriptionButtons = document.querySelectorAll('#read-description-button');
+readDescriptionButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const section = button.closest('.post');
+    const descriptionElement = section.querySelector('.text-description');
+    const utterance = new SpeechSynthesisUtterance(descriptionElement.textContent);
+    
+    if (synth.speaking) {
+      synth.cancel();
+    } else {
+      synth.speak(utterance);
+    }
+  });
+});
+
+const readContainmentButton = document.getElementById('read-containment-button');
+readContainmentButton.addEventListener('click', () => {
+  const containmentElement = document.querySelector('.text-description p:nth-of-type(3)');
+  const utterance = new SpeechSynthesisUtterance(containmentElement.textContent);
+  
+  if (synth.speaking) {
+    synth.cancel();
+  } else {
+    synth.speak(utterance);
+  }
+});
+
+
+
